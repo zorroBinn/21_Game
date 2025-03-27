f42955170e22c823e1689cea7dce444bbfc23944
Иван Соснин <zorrobinnaro@mail.ru>
2025-03-27 21:07:47 +0700

    Добавил поля Suit (enum class) масть и Rank (enum class) достоинство классу Карта и конструктор с параметрами (Suit s, Rank r). Тест создания шестёрки черви проходит

--- 21_Game/Card.h
+++ 21_Game/Card.h
@@ -4,6 +4,15 @@
 class Card
 {
 public:
+    enum class Suit { Hearts };
+    enum class Rank { Six = 6 };
+
+private:
+    Suit suit; //Масть
+    Rank rank; //Достоинство
+
+public:
     Card();
+    Card(Suit s, Rank r);
 };
 
