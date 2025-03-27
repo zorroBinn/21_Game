199078301c4cbdd8e2d9c68237f9db00a2c7cfda
Иван Соснин <zorrobinnaro@mail.ru>
2025-03-27 21:08:39 +0700

    Cdoc for Card(Suit s, Rank r)

--- 21_Game/Card.cpp
+++ 21_Game/Card.cpp
@@ -4,6 +4,11 @@ Card::Card()
 {
 }
 
+/// <summary>
+/// Конструктор с параметрами
+/// </summary>
+/// <param name="s"> Масть </param>
+/// <param name="r"> Достоинство </param>
 Card::Card(Suit s, Rank r)
 {
 	//todo реализовать присвоение карте масти и достоинства
