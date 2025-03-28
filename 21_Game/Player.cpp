382629bbc09c3aa9326e32c0283c1242dd8adc4a
Иван Соснин <zorrobinnaro@mail.ru>
2025-03-29 00:53:25 +0700

    Рефакторинг showHand() для прохождения теста playerShowHandWithFourCardsTest

--- 21_Game/Player.cpp
+++ 21_Game/Player.cpp
@@ -28,8 +28,11 @@ int Player::calculatePoints() const
 
 void Player::showHand() const
 {
-	//todo реализовать полноценный вывод карт игрока с подсчётом очков
-	std::cout << "Карты игрока Иван: A_Черви (11)" << std::endl;
+    std::cout << "Карты игрока " << name << ": ";
+    for (const auto& card : hand) {
+        card.print();
+    }
+    std::cout << "(" << calculatePoints() << ")" << std::endl;
 }
 
 std::string Player::getName() const
