1c8e7027b712e4f593ee0be0c1fa2e3873f9d961
Иван Соснин <zorrobinnaro@mail.ru>
2025-03-28 21:23:01 +0700

    Добавил класс игрока с конструктором, playerSimpleCreation Test проходит

---
+++ 21_Game/Player.cpp
@@ -0,0 +1,5 @@
+#include "Player.h"
+
+Player::Player(std::string playerName)
+{
+}
---
+++ 21_Game/Player.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+
+class Player
+{
+public:
+	Player(std::string playerName);
+};
+
