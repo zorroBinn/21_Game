664db18c8bb3be587e0a01b1d58b5a719c4540f0
Иван Соснин <zorrobinnaro@mail.ru>
2025-03-28 23:24:08 +0700

    Тест Player.getName() с проверкой уникальности возвращаемого имени

--- 21_Test/PlayerTest.cpp
+++ 21_Test/PlayerTest.cpp
@@ -14,3 +14,16 @@ TEST(PlayerTest, playerGetNameTest) {
     EXPECT_EQ(player->getName(), name);
     delete player;
 }
+
+TEST(PlayerTest, playerGetUniqueNameTest) {
+    string name1 = "name1";
+    string name2 = "name1";
+    string name3 = "name1";
+    Player* player1 = new Player(name1);
+    Player* player2 = new Player(name2);
+    Player* player3 = new Player(name3);
+    EXPECT_EQ(player1->getName(), name1) << "Первое имя не совпало";
+    EXPECT_EQ(player2->getName(), name2) << "Второе имя не совпало";
+    EXPECT_EQ(player3->getName(), name3) << "Третье имя не совпало";
+    delete player1, player2, player3;
+}
