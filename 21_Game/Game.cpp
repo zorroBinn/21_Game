75b9d76635f955e280f7a858ba74a16e42ad0e31
Иван Соснин <zorrobinnaro@mail.ru>
2025-03-29 15:59:01 +0700

    Рефакторинг addPlayer() и getPlayers() с тривиальными реализациями

--- 21_Game/Game.cpp
+++ 21_Game/Game.cpp
@@ -3,3 +3,15 @@
 Game::Game()
 {
 }
+
+void Game::addPlayer(Player p)
+{
+	//todo реализовать полноценное добавление игроков
+}
+
+std::vector<Player> Game::getPlayers() const
+{
+	//todo реализовать полноценный возврат вектора игроков
+	std::vector<Player> pl = { Player("Alice"), Player("Bob") };
+	return pl;
+}
--- 21_Game/Game.h
+++ 21_Game/Game.h
@@ -1,9 +1,13 @@
 #pragma once
-
+#include "Player.h"
+#include <vector>
 
 class Game
 {
 public:
 	Game();
+
+	void addPlayer(Player p);
+	std::vector<Player> getPlayers() const;
 };
 
