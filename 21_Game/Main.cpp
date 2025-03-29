1edaefc24301876e11f6db66a8cde0e52ed78697
Иван Соснин <zorrobinnaro@mail.ru>
2025-03-29 23:04:28 +0700

    Новый main файл

--- 21_Game/Main.cpp
+++ 21_Game/Main.cpp
@@ -1,7 +1,13 @@
 #include <Windows.h>
+#include "Game.h"
 
 int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
+	Game game;
+	game.setupGame();
+	game.play();
+	system("cls");
+	game.determineWinner();
 	return 0;
 }
