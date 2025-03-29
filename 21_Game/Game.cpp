38a39ab1a6d78de55d60c2e8f84357d3645e4308
Иван Соснин <zorrobinnaro@mail.ru>
2025-03-29 17:52:55 +0700

    Рефакторинг setupGame() для прохождения теста setupGameValidInputAndCheckFourPlayersTest

--- 21_Game/Game.cpp
+++ 21_Game/Game.cpp
@@ -7,7 +7,13 @@ Game::Game()
 void Game::setupGame()
 {
 	int numPlayers;
+	std::cout << "¬ведите количество игроков (2-4): ";
 	std::cin >> numPlayers;
+
+	while (numPlayers < 2 || numPlayers > 4) {
+		std::cout << "ќшибка! ¬ведите число от 2 до 4: ";
+		std::cin >> numPlayers;
+	}
 	for (int i = 1; i <= numPlayers; i++) {
 		std::string name;
 		std::cin >> name;
