35a176edf46506f0f66ce07326c88bf20b0387f5
Иван Соснин <zorrobinnaro@mail.ru>
2025-03-29 16:04:41 +0700

    Тест addPlayer() и getPlayers() с 3 игроками и проверкой их имени

--- 21_Test/GameTest.cpp
+++ 21_Test/GameTest.cpp
@@ -16,3 +16,17 @@ TEST(GameTest, gameAddAndGetPlayers) {
 
     EXPECT_EQ(game.getPlayers().size(), 2) << "Не 2 игрока";
 }
+
+TEST(GameTest, gameAddAndGetThreePlayersTest) {
+    Game game;
+
+    game.addPlayer(Player("name1"));
+    game.addPlayer(Player("name2"));
+    game.addPlayer(Player("name3"));
+    const auto& players = game.getPlayers();
+
+    EXPECT_EQ(players.size(), 3) << "Не 3 игрока";
+    EXPECT_EQ(players[0].getName(), "name1") << "Имя первого игрока не совпало";
+    EXPECT_EQ(players[1].getName(), "name2") << "Имя второго игрока не совпало";
+    EXPECT_EQ(players[2].getName(), "name3") << "Имя третьего игрока не совпало";
+}
